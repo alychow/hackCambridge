5fe0309e04b68989e16b98042e9bb3f009257aab
Raymond Chou <raymond.chou@mail.utoronto.ca>
2017-01-29 09:26:51 +0000

    edit node.js

--- chat/index.js
+++ chat/index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
@@ -10,9 +10,6 @@ io.on('connection', function(socket){
   console.log('a user connected');
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
 
 io.on('connection', function(socket){
   console.log('a user connected');
